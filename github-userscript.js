8eeddd40d56dc65dbf4701b49d205fc3f273bccc
Patrick Kik <patrick.kik@trifork.nl>
2017-10-24 20:43:23 +0200

    First version

---
+++ github-userscript.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name         github-userscript
+// @namespace    nl.patrickkik
+// @version      1.0
+// @description  Tampermonkey/Greasemonkey userscripts to modify GitHub to my preferences.
+// @author       Patrick Kik
+// @match        https://github.com/*/pull/*/files
+// @updateURL    https://raw.githubusercontent.com/PatrickKik/github-userscripts/master/github-userscript.js
+// @downloadURL  https://raw.githubusercontent.com/PatrickKik/github-userscripts/master/github-userscript.js
+// @grant        none
+// ==/UserScript==
+
+$(document).ready(buildMenu);
+
+function buildMenu() {
+    var divPrReviewTools = $("div.pr-review-tools")[0];
+    divPrReviewTools.appendChild(createToggleCollapseAllButton());
+}
+
+function toggleCollapseAll() {
+    $("button.btn-octicon").click();
+}
+
+function createToggleCollapseAllButton() {
+    var button = document.createElement("button");
+    button.setAttribute("id", "nl-patrickkik-toggle-collapse-all");
+    button.setAttribute("type", "button");
+    button.innerText = "Toggle collapse all";
+    button.style.marginLeft = "1em";
+    button.onclick = toggleCollapseAll;
+    return button;
+}
+
